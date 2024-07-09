328856853e69ea88e86a487f991ba0e5aa9ab1b7
a981008 <2587450776@qq.com>
2024-07-09 18:38:11 +0800

    第一次提交

---
+++ xcall.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+pcount=$#
+if((pcount==0));
+then
+        echo "command can not be null !"
+        exit
+fi
+
+
+user=`whoami`
+
+for ((i = 1; i <= 3; i++))
+do
+        echo ---------------- node0$i ----------------
+        ssh $user@node0$i 'source /etc/profile;'$@
+done
+
+echo --------------- complete ---------------
+
