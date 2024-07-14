6e7d5a583ee7d45fb67d3bd48755c5c4d60b98ca
a981008 <2587450776@qq.com>
2024-07-14 15:28:34 +0800

    KNOWN_HOSTS 去重

--- batch_ssh_auth.sh
+++ batch_ssh_auth.sh
@@ -10,7 +10,9 @@ declare -A HOSTS=(
 
 # 本地和远程的 SSH 密钥文件路径
 AUTHORIZED_KEYS="$HOME/.ssh/authorized_keys"
-TEMP_KEYS_FILE="/tmp/authorized_keys"
+TEMP_AUTHORIZED_KEYS="/tmp/authorized_keys"
+KNOWN_HOSTS="$HOME/.ssh/known_hosts"
+TMP_KNOWN_HOSTS="$HOME/tmp/known_hosts"
 
 auto_ssh_passwd() {
   local CMD=$1
@@ -42,8 +44,8 @@ generate_and_collect_keys() {
 distribute_keys() {
   for host in "${!HOSTS[@]}"; do
     echo "将公钥分发到 $host..."
-    auto_ssh_passwd "scp all_keys.pub $host:$TEMP_KEYS_FILE" ${HOSTS[$host]} >/dev/null 2>&1
-    auto_ssh_passwd "ssh $host \"cat $TEMP_KEYS_FILE >> $AUTHORIZED_KEYS && rm $TEMP_KEYS_FILE\"" ${HOSTS[$host]} >/dev/null 2>&1
+    auto_ssh_passwd "scp all_keys.pub $host:$TEMP_AUTHORIZED_KEYS" ${HOSTS[$host]} >/dev/null 2>&1
+    auto_ssh_passwd "ssh $host \"cat $TEMP_AUTHORIZED_KEYS >> $AUTHORIZED_KEYS && rm $TEMP_AUTHORIZED_KEYS\"" ${HOSTS[$host]} >/dev/null 2>&1
   done
 }
 
@@ -53,7 +55,7 @@ add_known_hosts() {
     for target in "${!HOSTS[@]}"; do
       host_ip=$(echo $target | cut -d'@' -f2)
       echo "将 $target 添加到 $source 的 known_hosts 中..."
-      ssh $source " ssh-keyscan -H $host_ip >> /root/.ssh/known_hosts" >/dev/null 2>&1
+      ssh $source " ssh-keyscan -H $host_ip >> $KNOWN_HOSTS && sort $KNOWN_HOSTS | uniq > $TMP_KNOWN_HOSTS && mv $TMP_KNOWN_HOSTS $KNOWN_HOSTS" >/dev/null 2>&1
     done
   done
 }
